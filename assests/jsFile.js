f876bb6498eef0136b8eace68f038dddb484bdb4
Tushar Tharwani <ttushaar45@gmail.com>
2023-08-20 01:41:19 +0530

    news fetching

--- assests/jsFile.js
+++ assests/jsFile.js
@@ -7,4 +7,41 @@ async function fetchNews (query){
    const res = await fetch(`${url}${query}&apikey=${api_keys}`)
    const data = await res.json();
    console.log(data)
+   bindData(data.articles);
+}
+function bindData(articles){
+   const cardContainer = document.getElementById('card-container');
+   const newsCardsTempalte = document.getElementById('template-card') 
+   
+   cardContainer.innerHTML='';
+   
+   articles.forEach((article) =>{
+      if(!article.urlToImage)return;
+      const cardClone = newsCardsTempalte.content.cloneNode(true)
+      fillDataInClone(cardClone,article)
+      cardContainer.appendChild(cardClone);
+   });
+}
+function fillDataInClone(cardClone,article){
+   const newImg = cardClone.querySelector('#news-img');
+   const newTitle = cardClone.querySelector('#news-title');
+   const newDesc = cardClone.querySelector('#news-desc');
+   const newSource = cardClone.querySelector('#news-source');
+
+   newImg.src = article.urlToImage;
+   newTitle.innerHTML = article.title;
+   newDesc.innerHTML = article.description;
+
+   const date = new Date(article.publishedAt).toLocaleString("en-US",{
+      timeZone:'Asia/Jakarta',
+   });
+   newSource.innerHTML = `${article.source.name}.${date}`;
+
+   cardClone.firstElementChild.addEventListener('click',()=>{
+      window.open(article.url,"_blank");
+   })
+}
+
+function onnavItemclick(id){
+   fetchNews(id)
 }
